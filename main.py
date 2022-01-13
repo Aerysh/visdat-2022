72867378c71932ee1b8366346c481edf77dccc70
Ahmad Azwar Annas <ahmadazw2@gmail.com>
2022-01-13 18:47:34 +0700

    hampir selesai

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from bokeh.core.property.dataspec import Value
-from bokeh.events import ButtonClick
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from bokeh.io import curdoc
+from bokeh.plotting import figure, show
+from bokeh.models import Select
+from bokeh.events import ButtonClick
+from bokeh.models.widgets import Button
 
 df = pd.read_csv("covid_19_indonesia_time_series_all.csv")
 
@@ -11,71 +12,63 @@ df['Date'] = pd.to_datetime(df['Date'])
 
 df.drop(df.index[df['Location'] == 'Indonesia'], inplace=True)
 
-from bokeh.plotting import figure, show
-from bokeh.layouts import widgetbox
-from bokeh.models.widgets import Dropdown, Button
-
 provinsi = df['Location']
 provinsi = provinsi.drop_duplicates()
 provinsi_list = provinsi.to_list()
 
 df.set_index('Date', inplace=True)
 
-from bokeh.plotting import figure, show
-from bokeh.layouts import widgetbox
-from bokeh.models.widgets import Dropdown
-
 tahun_list=['2020','2021']
 bulan_list=['1','2','3','4','5','6','7','8','9','10','11','12']
 y_list=['Total Cases','Total Deaths','Total Recovered']
 
-dropdown = Dropdown(label="Daftar Provinsi", button_type="warning", menu=provinsi_list, value='DKI Jakarta')
-dropdown2 = Dropdown(label="Tahun", button_type="warning", menu=tahun_list, value='2021')
-dropdown3 = Dropdown(label="Bulan", button_type="warning", menu=bulan_list, value='1')
-dropdown4 = Dropdown(label="Y-Label", button_type="warning", menu=y_list, value='Total Cases')
+select_provinsi = Select(options=provinsi_list, value='DKI Jakarta')
+select_tahun = Select(options=tahun_list, value='2020')
+select_bulan = Select(options=bulan_list, value='3')
+select_y = Select(options=y_list, value='Total Cases')
 button = Button(label="Show Plot")
 
 def select_df (kota,tahun,bulan,y):
-    tahun = int(tahun)
-    bulan = int(bulan)
     tmp = df.loc[df['Location'] == kota]
     tmp = tmp.loc[(tmp.index >= pd.Timestamp(tahun,bulan,1)) & (tmp.index < pd.Timestamp(tahun,bulan+1,1))]
     return tmp[[y]]
 
-temp_df = select_df('DKI Jakarta','2020','3','Total Cases')
-
 def show_plot(kota, tahun, bulan ,y):
     temp_df = select_df(kota, tahun, bulan ,y)
 
-    p = figure(title="Covid Case", x_axis_label='Date', y_axis_label='Total Case')
+    p = figure(title="Covid Case", x_axis_label='Date', y_axis_label=y)
 
-    p.line(temp_df.index, temp_df['Total Cases'], legend_label="Temp.", line_width=2)
+    p.line(temp_df.index, temp_df[y], legend_label="Temp.", line_width=2)
 
     curdoc().clear()
-    curdoc().add_root(dropdown)
-    curdoc().add_root(dropdown2)
-    curdoc().add_root(dropdown3)
-    curdoc().add_root(dropdown4)
+    curdoc().add_root(select_provinsi)
+    curdoc().add_root(select_tahun)
+    curdoc().add_root(select_bulan)
+    curdoc().add_root(select_y)
     curdoc().add_root(button)
     curdoc().add_root(p)
 
-def handler(event):
-    return event.item
-def handler2(event):
-    return event.item
-def handler3(event):
-    return event.item
-def handler4(event):
-    return event.item
+def handler(attr, old, new):
+    return select_provinsi.value
+def handler2(attr, old, new):
+    return int(select_tahun.value)
+def handler3(attr, old, new):
+    return int(select_bulan.value)
+def handler4(attr, old, new):
+    return select_y.value
 
-a = dropdown.on_click(handler)
-b = dropdown2.on_click(handler2)
-c = dropdown3.on_click(handler3)
-d = dropdown4.on_click(handler4)
-button.on_click(show_plot(a,b,c,d))
+a = select_provinsi.on_change('value', handler)
+b = select_tahun.on_change('value', handler2)
+c = select_bulan.on_change('value', handler3)
+d = select_y.on_change('value', handler4)
 
-curdoc().add_root(dropdown)
-curdoc().add_root(dropdown2)
-curdoc().add_root(dropdown3)
-curdoc().add_root(dropdown4)
+def callback(event):
+    show_plot(select_provinsi.value, int(select_tahun.value), int(select_bulan.value), select_y.value)
+
+button.on_event(ButtonClick, callback)
+
+curdoc().add_root(select_provinsi)
+curdoc().add_root(select_tahun)
+curdoc().add_root(select_bulan)
+curdoc().add_root(select_y)
 curdoc().add_root(button)
